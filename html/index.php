59afe11abbdf240ac905a6f6b6863850e7b66bd6
ayumu kawai <ayuayuziku@gmail.com>
2023-06-07 16:27:46 +0900

    UI の構築

--- html/index.php
+++ html/index.php
@@ -16,7 +16,33 @@
 <body>
     <div class="container text-left w-50 mt-5">
         <h1 class="my-3">新人歓迎会参加アンケート結果</h1>
-        <a href="/add.php" class="btn btn-secondary">アンケートに回答する</a>
+
+        <div class="d-flex flex-row border-bottom my-3">
+            <div class="px-5">ID</div>
+            <div class="px-5">氏名</div>
+            <div class="px-5">参加するかどうか</div>
+            <div class="px-5">コメント</div>
+        </div>
+        <div class="d-flex flex-row my-1">
+            <div class="px-5">1</div>
+            <div class="px-5">川井歩</div>
+            <div class="px-5">参加</div>
+            <div class="px-5">楽しみにしております。</div>
+        </div>
+        <div class="d-flex flex-row my-1">
+            <div class="px-5">2</div>
+            <div class="px-5">海色カイ</div>
+            <div class="px-5">不参加</div>
+            <div class="px-5">仕事が忙しいです。</div>
+        </div>
+        <div class="d-flex flex-row my-1">
+            <div class="px-5">3</div>
+            <div class="px-5">空色そら</div>
+            <div class="px-5">参加</div>
+            <div class="px-5"></div>
+        </div>
+
+        <a href="/add.php" class="btn btn-secondary my-2">アンケートに回答する</a>
     </div>
 </body>
 
