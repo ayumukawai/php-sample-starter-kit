60f2b35f3deda02dd542800264397cd8a188fcd2
ayumu kawai <ayuayuziku@gmail.com>
2023-06-07 15:24:15 +0900

    追記

--- html/add.php
+++ html/add.php
@@ -50,6 +50,7 @@ if ($_SERVER['REQUEST_METHOD'] === "POST") {
       <button type="submit">送信</button>
     </div>
   </form>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
 
 </html>
