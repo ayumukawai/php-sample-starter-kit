7202ee41ba2fa746362d91802696fd6e2c0c17fd
ayumu kawai <ayuayuziku@gmail.com>
2023-06-26 16:48:26 +0900

    微修正

--- html/validation.php
+++ html/validation.php
@@ -1,18 +1,15 @@
 <?php
 
-function validation()
-{
-    $errors = [];
+$errors = [];
 
-    if (isset($_POST)) {
-        if (empty($_POST['username'])) {
-            $errors[] = "氏名は必須項目です。";
-        } else if (mb_strlen($_POST['username']) > 20) {
-            $errors[] = "氏名は20文字以内で入力して下さい。";
-        }
+if (isset($_POST)) {
+    if (empty($_POST['username'])) {
+        $errors[] = "氏名は必須項目です。";
+    } else if (mb_strlen($_POST['username']) > 20) {
+        $errors[] = "氏名は20文字以内で入力して下さい。";
+    }
 
-        if (mb_strlen($_POST['comment']) > 100) {
-            $errors[] = "コメントは100文字以内で入力して下さい。";
-        }
+    if (mb_strlen($_POST['comment']) > 100) {
+        $errors[] = "コメントは100文字以内で入力して下さい。";
     }
 }
